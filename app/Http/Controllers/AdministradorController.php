b1c1043c52d73b4d5742137e252dad391443b4f7
luis.pineda <luis.pineda@ceindetec.org.co>
2016-10-26 11:07:17 -0500

    molestando con el dataTable

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -138,12 +138,106 @@ class AdministradorController extends Controller
      */
     public function publicPendientes()
     {
-        return $this->validatesRequestErrorBag;
+        return view('Administrador.publicPendientes');
     }
 
 
+    /**
+     * @return string
+     */
+    public function pendientes(Request $request)
+    {
 
+        $data = array();
 
+        $data["draw"]=1;
+        $data["recordsTotal"]=57;
+        $data["recordsFiltered"]=57;
+        $data["data"]=[
+            [
+                "Airi",
+                "Satou",
+                "Accountant",
+                "Tokyo",
+                "28th Nov 08",
+                "$162,700"
+            ],
+            [
+                "Angelica",
+                "Ramos",
+                "Chief Executive Officer (CEO)",
+                "London",
+                "9th Oct 09",
+                "$1,200,000"
+            ],
+            [
+                "Ashton",
+                "Cox",
+                "Junior Technical Author",
+                "San Francisco",
+                "12th Jan 09",
+                "$86,000"
+            ],
+            [
+                "Bradley",
+                "Greer",
+                "Software Engineer",
+                "London",
+                "13th Oct 12",
+                "$132,000"
+            ],
+            [
+                "Brenden",
+                "Wagner",
+                "Software Engineer",
+                "San Francisco",
+                "7th Jun 11",
+                "$206,850"
+            ],
+            [
+                "Brielle",
+                "Williamson",
+                "Integration Specialist",
+                "New York",
+                "2nd Dec 12",
+                "$372,000"
+            ],
+            [
+                "Bruno",
+                "Nash",
+                "Software Engineer",
+                "London",
+                "3rd May 11",
+                "$163,500"
+            ],
+            [
+                "Caesar",
+                "Vance",
+                "Pre-Sales Support",
+                "New York",
+                "12th Dec 11",
+                "$106,450"
+            ],
+            [
+                "Cara",
+                "Stevens",
+                "Sales Assistant",
+                "New York",
+                "6th Dec 11",
+                "$145,600"
+            ],
+            [
+                "Cedric",
+                "Kelly",
+                "Senior Javascript Developer",
+                "Edinburgh",
+                "29th Mar 12",
+                "$433,060"
+            ]
+        ];
+
+        return $data ;
+    }
 
 
 
